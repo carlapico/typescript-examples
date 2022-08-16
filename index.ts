51da4c6daeb712afe0c528119a1770b0972eda8c
Carla Pico <carlaspico@gmail.com>
2022-08-16 14:59:12 -0400

    added in a function

--- index.ts
+++ index.ts
@@ -1,26 +1,26 @@
-let firstName:string = "Carla"
-let age:number = 24
-let active:boolean = true
+// let firstName:string = "Carla"
+// let age:number = 24
+// let active:boolean = true
 
-// let names:string[] = [ 1, 2, 3, 4] // this is valid in Javascript but it Typescript it is invalid
-let names:string[] = [ 'bobby', 'jennifer', 'lily'] // good array
+// // let names:string[] = [ 1, 2, 3, 4] // this is valid in Javascript but it Typescript it is invalid
+// let names:string[] = [ 'bobby', 'jennifer', 'lily'] // good array
 
-console.log(names)
-console.log(`My first name is ${firstName}`)
+// console.log(names)
+// console.log(`My first name is ${firstName}`)
 
 
-enum studentTypes {
-    Pending, 
-    Active, 
-    AlmostDone,
-    Graduated, 
-    TAs, 
-    Teachers
-}
+// enum studentTypes {
+//     Pending, 
+//     Active, 
+//     AlmostDone,
+//     Graduated, 
+//     TAs, 
+//     Teachers
+// }
 
-console.log(studentTypes) // tells you what the numbers are assigned to each of the enums
+// console.log(studentTypes) // tells you what the numbers are assigned to each of the enums
 
-const myStatus = studentTypes.Graduated
+// const myStatus = studentTypes.Graduated
 
 // switch(myStatus) {
 //     case studentTypes.Active:
@@ -33,8 +33,16 @@ const myStatus = studentTypes.Graduated
 // }
 
 
-let code: string | number = 11
-code = "AA11" // now it is changed to a string because that is a valid value type. 
-// code = false // not a valid value type unless we add boolean to the typecasting
+// let code: string | number = 11
+// code = "AA11" // now it is changed to a string because that is a valid value type. 
+// // code = false // not a valid value type unless we add boolean to the typecasting
 
-console.log(`My entry code is: ${code}`)
+// console.log(`My entry code is: ${code}`)
+
+function sum (arr: number[]) {
+    return arr.reduce((total:number, num:number) => total + num )
+}
+
+let numbers: number[] = [ 1, 2, 3, 4, 5, 6 ]
+
+console.log(sum(numbers))
