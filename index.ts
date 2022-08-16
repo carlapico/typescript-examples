02b9c946deaa6fa0f69bc99688048c4c854d0dee
Carla Pico <carlaspico@gmail.com>
2022-08-16 14:16:39 -0400

    added in an array in ts

--- index.ts
+++ index.ts
@@ -2,4 +2,8 @@ let firstName:string = "Carla"
 let age:number = 24
 let active:boolean = true
 
+// let names:string[] = [ 1, 2, 3, 4] // this is valid in Javascript but it Typescript it is invalid
+let names:string[] = [ 'bobby', 'jennifer', 'lily'] // good array
+
+console.log(names)
 console.log(`My first name is ${firstName}`)
