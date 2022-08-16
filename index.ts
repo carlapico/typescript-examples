83b060e616211002705846d8435f1964d222ebd7
Carla Pico <carlaspico@gmail.com>
2022-08-16 14:46:57 -0400

    added some type annotations with union and enums

--- index.ts
+++ index.ts
@@ -7,3 +7,34 @@ let names:string[] = [ 'bobby', 'jennifer', 'lily'] // good array
 
 console.log(names)
 console.log(`My first name is ${firstName}`)
+
+
+enum studentTypes {
+    Pending, 
+    Active, 
+    AlmostDone,
+    Graduated, 
+    TAs, 
+    Teachers
+}
+
+console.log(studentTypes) // tells you what the numbers are assigned to each of the enums
+
+const myStatus = studentTypes.Graduated
+
+// switch(myStatus) {
+//     case studentTypes.Active:
+//         break;
+// }
+
+// console.log(myStatus)
+// if (myStatus == studentTypes.Graduated) {
+//     console.log(`the student is  ${myStatus }`)
+// }
+
+
+let code: string | number = 11
+code = "AA11" // now it is changed to a string because that is a valid value type. 
+// code = false // not a valid value type unless we add boolean to the typecasting
+
+console.log(`My entry code is: ${code}`)
